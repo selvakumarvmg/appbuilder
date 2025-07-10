bf6f0932ff6dd32a0c1c8603acd15729d6904e11
selvakumar <selvakumar@vmgdigital.com>
2025-07-10 18:13:46 +0530

    mac 33

--- installer/dmg-settings.py
+++ installer/dmg-settings.py
@@ -1,40 +1,26 @@
 # installer/dmg-settings.py
 
 application = "PremediaApp"
-app_path = "dmg-build/PremediaApp.app"
 background = "installer-assets/dmg-background.bmp"
 
 volume_name = "PremediaApp"
 icon_size = 128
 window_rect = ((200, 200), (480, 360))
 
+files = [
+    ("dmg-build/PremediaApp.app", "PremediaApp.app"),
+]
+
 dmg_settings = {
-    # Volume name shown on mount
     "volume_name": volume_name,
-
-    # Optional icon for mounted volume
     "volume_icon": None,
-
-    # Background image path
     "background": background,
-
-    # App icon size in the DMG window
     "icon_size": icon_size,
-
-    # Window size and position
     "window_rect": window_rect,
-
-    # Creates shortcut to /Applications in DMG
     "applications_link": True,
-
-    # Icon positions inside DMG window
     "icon_locations": {
         application: (140, 160),
         "Applications": (340, 160)
     },
-
-    # List of actual files to include in the DMG
-    "files": [
-        (app_path, f"{application}.app")
-    ],
+    "files": files,
 }
