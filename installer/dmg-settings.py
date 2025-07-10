ffbe33e5e875f728fe89c124e8da94c9d920cc03
selvakumar <selvakumar@vmgdigital.com>
2025-07-10 15:29:21 +0530

    mac 16

--- installer/dmg-settings.py
+++ installer/dmg-settings.py
@@ -1,29 +1,26 @@
 # installer/dmg-settings.py
 
-# Required settings
 application = "PremediaApp"
-app_path = f"dmg-build/{application}.app"
+app_path = f"dmg-build/{application}.app"  # Path to your built .app bundle
 
-# Optional background image
-background = "installer-assets/dmg-background.bmp"
-
-# Volume name
-volume_name = "PremediaApp"
-
-# Icon location settings
+# DMG appearance
+volume_name = "PremediaApp"  # Shown when DMG is mounted
 icon_size = 128
+background = "installer-assets/dmg-background.bmp"  # Must be 72 DPI, BMP/PNG format, and exist
+
+# DMG window layout
+window_rect = ((100, 100), (520, 280))  # Position (x, y), Size (w, h)
+
+# Icon positions (relative to DMG window)
 icon_locations = {
     f"{application}.app": (140, 120),
     "Applications": (380, 120),
 }
 
-# Window size and placement
-window_rect = ((100, 100), (520, 280))  # (x, y), (width, height)
-
-# Code signing (disabled here)
-codesign_identity = None
-
-# Don't create symlinks
+# Add symlink to /Applications
 symlinks = {
     "Applications": "/Applications"
 }
+
+# Disable code signing
+codesign_identity = None
