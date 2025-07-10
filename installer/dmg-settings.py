ba1e381a11fb70dd45d545924dd13475ff94c123
selvakumar <selvakumar@vmgdigital.com>
2025-07-10 17:55:49 +0530

    mac 32

--- installer/dmg-settings.py
+++ installer/dmg-settings.py
@@ -9,13 +9,32 @@ icon_size = 128
 window_rect = ((200, 200), (480, 360))
 
 dmg_settings = {
+    # Volume name shown on mount
+    "volume_name": volume_name,
+
+    # Optional icon for mounted volume
     "volume_icon": None,
+
+    # Background image path
     "background": background,
+
+    # App icon size in the DMG window
     "icon_size": icon_size,
+
+    # Window size and position
     "window_rect": window_rect,
+
+    # Creates shortcut to /Applications in DMG
     "applications_link": True,
+
+    # Icon positions inside DMG window
     "icon_locations": {
         application: (140, 160),
         "Applications": (340, 160)
     },
+
+    # List of actual files to include in the DMG
+    "files": [
+        (app_path, f"{application}.app")
+    ],
 }
